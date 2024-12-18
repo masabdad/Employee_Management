3937e63a7a60850f1a620b79df3323cec6f8c09b
masabdad <mahsabdad@gmail.com>
2024-12-18 23:52:59 +0500

    Updated

--- src/main/java/com/employee/managment/Employee/Management/controller/ListF.java
+++ src/main/java/com/employee/managment/Employee/Management/controller/ListF.java
@@ -17,16 +17,9 @@ public class ListF {
     public static void main(String[] args) {
         float floatValue = 123.45f;
         String convertedString = String.valueOf(floatValue);
-
-        String[] splittedString = convertedString.split("\\.");
-
-        String stringBeforeDecimal = splittedString[0];
-        String stringsAfterDecimal =  splittedString[1];
-        String reversedStringFirstIndex = new StringBuilder(stringBeforeDecimal).reverse().toString();
-        String reversedStringSecondIndex = new StringBuilder(stringsAfterDecimal).reverse().toString();
-        String reversedFloat = reversedStringFirstIndex + "." + reversedStringSecondIndex;
-        System.out.println(reversedFloat);
-
-
+        String replace = convertedString.replace(".", "");
+        String reversedString = String.valueOf(new StringBuilder(replace).reverse());
+        String string = new StringBuilder(reversedString).insert(3, ".").toString();
+        System.out.println(string);
     }
 }
